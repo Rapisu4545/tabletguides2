c106c5e3e7cc25d49096079f0341e42ae22bcc95
alienshooter88 <41442700+alienshooter88@users.noreply.github.com>
2018-07-25 19:26:10 +0300

    24.07

---
+++ src/main/resources/script/files.js
@@ -0,0 +1,35 @@
+var control = document.getElementById("file");
+control.addEventListener("change", function(event) {
+    // Когда происходит изменение элементов управления, значит появились новые файлы
+    var i = 0,
+        files = control.files,
+        len = files.length;
+
+
+
+    for (; i < len; i++) {
+
+        var filename = files[i].name;
+        var reader = new FileReader();
+
+        reader.addEventListener("load", function(event) {
+            var list = document.querySelector('.viewfiles');
+            var card = document.createElement('div');
+            card.classList.add('fileelement');
+            list.appendChild(card);
+            var img = document.createElement('img');
+            img.src = event.target.result;
+            img.setAttribute('alt', 'image');
+            card.appendChild(img);
+            var imgbutton = document.createElement('input');
+            imgbutton.type = 'button';
+            imgbutton.classList.add('button-for-textarea');
+            imgbutton.classList.add('imgbutton');
+            card.appendChild(imgbutton);
+        });
+
+
+        reader.readAsDataURL(files[i]);
+    }
+
+}, false);
